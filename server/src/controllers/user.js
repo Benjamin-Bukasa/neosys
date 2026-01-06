032e93221cfc5c21d396565d49de1b1bf9e639c0
Benjamin-Kabeya <bukasabenjamin6@gmail.com>
2026-01-06 23:39:35 +0100

    feat: create user by admin controller

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,6 +1,33 @@
+import prisma from "../config/db.js"
 
-export const createUser = (req,res)=>{
-    res.send("user created successfully !")
+
+export const createUser = async (req,res)=>{
+    const {firstname,lastname, username, gender,dateBirth, role, email, password,} = req.body
+    
+    const emailExist = await prisma.user.findUnique({where:{email}})
+    if (emailExist) return res.status(400).json({ message: "Email already used" });
+
+    const hash = await bcrypt.hash(password, 12);
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+
+    await prisma.user.create({
+            data: { 
+                firstname,
+                lastname, 
+                username, 
+                phone, 
+                gender, 
+                dateBirth,
+                role,
+                email, 
+                password: hash, 
+                verificationToken }
+        });
+
+    await prisma.user.update({
+        where: { id: user.id },
+        data: { isVerified: true, verificationToken: null }
+      });
 }
 
 
